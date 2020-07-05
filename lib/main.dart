a4cab7794ba495dee0f254c35ea277443a6d4032
Santosh Sharma <santosh@thinkbiz.co.in>
2020-07-05 18:18:02 +0530

    learn flutter basics

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'mocks/mock_location.dart';
+import 'location_list.dart';
+
+void main() {
+  final mockLocations = MockLocation.fetchAll();
+
+  return runApp(MaterialApp(home: LocationList(mockLocations)));
+}
